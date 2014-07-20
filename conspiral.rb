3a9015d5c7f8048e0a8227b201420ef6d60acd66
Jônatas Paganini <jonatasdp@gmail.com>
2014-07-19 22:26:40 -0300

    fixing and extending examples..

--- conspiral.rb
+++ conspiral.rb
@@ -3,7 +3,7 @@ require "drawille"
 
 canvas = Drawille::Canvas.new
 f = Drawille::FlipBook.new
-size = 30 #30 
+size = 222
 distance = 1
 size = ARGV[0].to_i if ARGV[0]
 distance = ARGV[1].to_i if ARGV[1]
@@ -16,16 +16,15 @@ def fib(n)
     n + fib(n-1)
   end
 end
-fibo = [2,3,5,8,13,18,31,49]
 
 frame = canvas.paint do
-  move 0,0
+  move centerx,centery
   down
   size.times do |i|
     angle = 0.2 * i;
     x = centerx + (_a + _b * angle) * Math.cos(angle)
     y = centery + (_a + _b * angle) * Math.sin(angle)
-    if i > 0 and fibo[0,2].find{|n|i % n == 0}
+    if i > 0 and i % 5 == 0
       move centerx, centery
     else
       move x, y
--- splash.rb
+++ splash.rb
@@ -1,30 +1,39 @@
 require "rubygems"
-require "bundler/setup"
 require "drawille"
 
 canvas = Drawille::Canvas.new
 f = Drawille::FlipBook.new
-size = 100
+size = 222
 distance = 1
 size = ARGV[0].to_i if ARGV[0]
 distance = ARGV[1].to_i if ARGV[1]
 _a = _b = distance
-centery =  size / 2
-centerx = centery / 2
+centery = centerx =  size / 2 
+def fib(n)
+  if n == 1
+    1
+  else
+    n + fib(n-1)
+  end
+end
+fibo = [2,3,5,8,13,18,31,49] # numbers from nature
 
 frame = canvas.paint do
-  move centerx, centery
+  move centerx,centery
   down
   size.times do |i|
     angle = 0.2 * i;
     x = centerx + (_a + _b * angle) * Math.cos(angle)
     y = centery + (_a + _b * angle) * Math.sin(angle)
-    move x, y
-    down
+    if i > 0 and fibo.find{|n|i % n == 0}
+      move centerx, centery
+    else
+      move x, y
+    end
     f.snapshot canvas
   end
 end.frame
 
-f.play fps: 30
+f.play fps: 40
 
 puts frame
